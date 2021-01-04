c0828c646d5c8691bfbae4e11259c92544f0b9d6
namrouche993 <42832704+namrouche993@users.noreply.github.com>
2021-01-04 23:41:31 +0100

    Update app.R

--- app.R
+++ app.R
@@ -15,26 +15,28 @@ library(shinycssloaders)
 #library(rAmCharts)
 library(shinyBS)
 
-library(leaflet)
+# library(leaflet)
+
 #library(htmltools)
-library(leaflet.extras)
+
+# library(leaflet.extras)
 
 
-library(rgdal)
-library(sp)
-library(readxl)
-library(highcharter)
-library(tidyverse)
-library(excelR)
-library(farver)
-library(readxl)
+# library(rgdal)
+# library(sp)
+# library(readxl)
+# library(highcharter)
+# library(tidyverse)
+# library(excelR)
+# library(farver)
+# library(readxl)
 
 
-library(reactable)
+# library(reactable)
 #library(grDevices)
 #library(janitor)
 
-library(shinyjs)
+# library(shinyjs)
 
 
 # Define UI for application that draws a histogram
