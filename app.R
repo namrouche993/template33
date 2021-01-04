bb2dd2b5e444e92a5180b51cc50dfd4e53eee762
namrouche993 <42832704+namrouche993@users.noreply.github.com>
2021-01-04 22:00:34 +0100

    Update app.R

--- app.R
+++ app.R
@@ -18,7 +18,7 @@ plica1=rownames(plic)
 ui <- fluidPage(
     
     # Application title
-    titlePanel("Old Faithful USMALGERiiaae Geyjjjjjjjjjjjjjjser Data"),
+    titlePanel("Old Faithful USMALGsqaERiiaae Geyjjjjjjjjjjjjjjser Data"),
     
     # Sidebar with a slider input for number of bins
     sidebarLayout(
