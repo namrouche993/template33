c3a70dce8b3f3b29942bd1a89581dca4b9d53d39
namrouche993 <42832704+namrouche993@users.noreply.github.com>
2021-01-07 21:45:27 +0100

    Update app.R

--- app.R
+++ app.R
@@ -28,7 +28,7 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+highchartOutput("distPlot")
         )
     )
 )
@@ -36,13 +36,8 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$distPlot <- renderHighchart({
+    hchart(mtcars,'column',hcaes(x=mpg,y=cyl))
     })
 }
 
